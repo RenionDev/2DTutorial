a9cc3a6f55b0d2d24f8b2cc3eb8399a2c09a3ed5
Renion <lubustarcraft@gmail.com>
2025-09-28 02:09:36 +0200

    feat: Connect hud to main scene

--- Main.cs
+++ Main.cs
@@ -14,6 +14,8 @@ public partial class Main : Node
     {
         GetNode<Timer>("MobTimer").Stop();
         GetNode<Timer>("ScoreTimer").Stop();
+
+        GetNode<Hud>("HUD").ShowGameOver();
     }
 
     public void NewGame()
@@ -25,12 +27,17 @@ public partial class Main : Node
         player.Start(startPosition.Position);
 
         GetNode<Timer>("StartTimer").Start();
+
+        var hud = GetNode<Hud>("HUD");
+        hud.UpdateScore(_score);
+        hud.ShowMessage("Get Ready");
     }
 
     // We also specified this function name in PascalCase in the editor's connection window.
     public void OnScoreTimerTimeout()
     {
         _score++;
+        GetNode<Hud>("HUD").UpdateScore(_score);
     }
     
     // We also specified this function name in PascalCase in the editor's connection window.
@@ -71,6 +78,5 @@ public partial class Main : Node
 
     public override void _Ready()
     {
-        NewGame();
     }
 }
